96fa24c363a048cb5163e2dad8d56b341bc2aee5
Anas Qadrei <anasqadrei@gmail.com>
2016-12-29 18:09:53 +1100

    live config

--- app/src/main/java/com/awtarika/android/app/util/Constants.java
+++ app/src/main/java/com/awtarika/android/app/util/Constants.java
@@ -7,11 +7,10 @@ package com.awtarika.android.app.util;
 public class Constants {
     public class URLs {
         public final static String PROTOCOL = "http";
-//        public final static String HOST = "awtphase2dev.herokuapp.com";
-        public final static String HOST = "192.168.10.2:3000";
+        public final static String HOST = "www.awtarika.com";
     }
 
     public class LogEntries {
-        public final static String TOKEN = "5321e40a-65f7-40d1-b677-188fbbec5dd3";
+        public final static String TOKEN = "3e5472c3-77ff-431b-bee2-4493723af646";
     }
 }
